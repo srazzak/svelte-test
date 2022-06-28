e12ca5e9689c6fa41269c7301e7e298e40d7da62
srazzak <suhail.razzak@gmail.com>
2022-06-28 16:37:54 -0400

    fix

--- svelte-components/src/main.js
+++ svelte-components/src/main.js
@@ -1,15 +1,7 @@
-import App from './App.svelte';
 import HelloWorld from './HelloWorld.svelte';
 
 new HelloWorld({
-	target: document.querySelector('#test')
+	target: document.querySelector('#hello-world')
 });
 
-const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
-});
-
-export default app;
+export default HelloWorld;
